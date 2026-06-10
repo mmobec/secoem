31b4e554a68cabc68dbdb2b2d0ca9ff42b9bc6dd
„Jan <janjettmann@me.com>
2026-06-10 18:03:15 +0200

    Now reading positive and negative imbalance prices separately

--- codes/ec_run.py
+++ codes/ec_run.py
@@ -506,14 +506,12 @@ for sim in SIMS:
     # Compute market prices (Day-Ahead prices already defined before creating the instance)
     lR_dict = {}
     lI_dict = {}
-    lIB_dict = {}
     # Assign values to dictionaries
     for t in instance.T:
         for s in instance.S:
             # Reserve market prices
             lR_dict[t, s] = value(instance.Scen[value(instance.nT) + t, s])
-            # System imbalance prices
-            lIB_dict[t, s] = value(instance.Scen[value(instance.fRVSG[value(instance.nSG)]) + (t - 1), s])
+
     # Assign Intraday Market prices
     for i in instance.IM:
         for t in instance.TIM[i]:
@@ -524,8 +522,31 @@ for sim in SIMS:
         instance.lR[t, s] = val
     for (i, t, s), val in lI_dict.items():
         instance.lI[i, t, s] = val
-    for (t, s), val in lIB_dict.items():
-        instance.lIB[t, s] = val
+
+        ib_stage = scenario_data["nSG"]  # Assuming imbalance market is at the last stage
+        ib_nrvsg = sum(int(nrvsg.get(sg, 0)) for sg in range(1, int(ib_stage)))
+        nT = int(scenario_data["nT"])
+        lPIB_dict = {}
+        lNIB_dict = {}
+        for t in range(1, nT + 1):
+            for s in S_preserved:
+                lPIB_dict[(t, s)] = float(scenario_data.data()["Scen"].get((ib_nrvsg + t -1, s), 0.0))
+                lNIB_dict[(t, s)] = float(scenario_data.data()["Scen"].get((ib_nrvsg + t -1 + nT, s), 0.0))
+
+        for (t, s), val in lPIB_dict.items():
+            instance.lPIB[t, s] = val
+        for (t, s), val in lNIB_dict.items():
+            instance.lNIB[t, s] = val
+        #scenario_data.data()["lPIB"] = lPIB_dict
+        #scenario_data.data()["lNIB"] = lNIB_dict
+        mean_lPIB_dict = {t: sum(value(instance.Prob[s]) * lPIB_dict[(t, s)] for s in instance.S) for t in instance.T }
+        mean_lNIB_dict = {t: sum(value(instance.Prob[s]) * lNIB_dict[(t, s)] for s in instance.S) for t in instance.T }
+
+       # Store computed values in Pyomo model
+        for t, val in mean_lPIB_dict.items():
+            instance.mean_lPIB[t] = val
+        for t, val in mean_lNIB_dict.items():
+            instance.mean_lNIB[t] = val
 
     # Compute mean market prices (the average value across all scenarios for each hour)
     mean_lD_dict = {}
@@ -536,12 +557,14 @@ for sim in SIMS:
     for t in instance.T:
         mean_lD_dict[t]  = sum(value(instance.Prob[s]) * value(instance.lD[t, s]) for s in instance.S)
         mean_lR_dict[t]  = sum(value(instance.Prob[s]) * value(instance.lR[t, s]) for s in instance.S)
-        mean_lIB_dict[t] = sum(value(instance.Prob[s]) * value(instance.lIB[t, s]) for s in instance.S)
+        #mean_lIB_dict[t] = sum(value(instance.Prob[s]) * value(instance.lIB[t, s]) for s in instance.S)
     # Compute mean values for intraday markets
     for i in instance.IM:
         for t in instance.TIM[i]:
             mean_lI_dict[i, t] = sum(value(instance.Prob[s]) * value(instance.lI[i, t, s]) for s in instance.S)
     
+
+
     # Store values in Pyomo instance
     for t, val in mean_lD_dict.items():
         instance.mean_lD[t] = val
@@ -549,8 +572,8 @@ for sim in SIMS:
         instance.mean_lR[t] = val
     for (i, t), val in mean_lI_dict.items():
         instance.mean_lI[i, t] = val 
-    for t, val in mean_lIB_dict.items():
-        instance.mean_lIB[t] = val
+    #for t, val in mean_lIB_dict.items():
+        #instance.mean_lIB[t] = val
         
     
     # === PRINT RESULTS ===
@@ -575,8 +598,9 @@ for sim in SIMS:
     # Compute and display mean values (the average value across all scenarios for all hours)
     mean_lD_avg  = sum(mean_lD_dict[t] for t in instance.T) / value(instance.nT)
     mean_lR_avg  = sum(mean_lR_dict[t] for t in instance.T) / value(instance.nT)
-    mean_lIB_avg = sum(mean_lIB_dict[t] for t in instance.T) / value(instance.nT)
-
+    # Compute the average values
+    mean_lPIB_avg = sum(mean_lPIB_dict[t] for t in instance.T) / value(instance.nT)
+    mean_lNIB_avg = sum(mean_lNIB_dict[t] for t in instance.T) / value(instance.nT)
     # Compute average per intraday market (per i)
     mean_lI_avg_per_market = {}
     for i in instance.IM:
@@ -593,28 +617,12 @@ for sim in SIMS:
         res_out.write("\nMean Values:\n")
         res_out.write(f"mean_lD_avg  = {mean_lD_avg:.6f}\n")
         res_out.write(f"mean_lR_avg  = {mean_lR_avg:.6f}\n")
-        res_out.write(f"mean_lIB_avg = {mean_lIB_avg:.6f}\n")
+        res_out.write(f"mean_lIB_avg = {mean_lPIB_avg:.6f}\n")
+        res_out.write(f"mean_lIB_avg = {mean_lNIB_avg:.6f}\n")
         res_out.write(f"mean_pW_avg  = {mean_pW_avg:.6f}\n")
         res_out.write(f"mean_pPV_avg  = {mean_pW_avg:.6f}\n")
     
-    # Compute positive and negative imbalance prices
-    lPIB_dict = {}
-    lNIB_dict = {}                   
-    for t in instance.T:
-        for s in instance.S:
-            lIB_val = value(instance.lIB[t, s])
-            lD_val = value(instance.lD[t, s])
-            if lIB_val <= 1:
-                lPIB_dict[(t, s)] = min(180.3, lIB_val * lD_val)
-                lNIB_dict[(t, s)] = lD_val
-            else:
-                lPIB_dict[(t, s)] = lD_val
-                lNIB_dict[(t, s)] = min(180.3, lIB_val * lD_val)
-    # Store computed values in Pyomo model
-    for (t, s), val in lPIB_dict.items():
-        instance.lPIB[t, s] = val
-    for (t, s), val in lNIB_dict.items():
-        instance.lNIB[t, s] = val
+
     
     # Compute mean values for imbalance prices
     mean_lPIB_dict = {t: sum(value(instance.Prob[s]) * lPIB_dict[(t, s)] for s in instance.S) for t in instance.T}
@@ -635,23 +643,20 @@ for sim in SIMS:
     # for t, val in mean_lNIB_dict.items():
     #     print(f"mean_lNIB[{t}] = {val}")
 
-    # Compute the average values
-    mean_lPIB_avg = sum(mean_lPIB_dict[t] for t in instance.T) / value(instance.nT)
-    mean_lNIB_avg = sum(mean_lNIB_dict[t] for t in instance.T) / value(instance.nT)
+
     
     # Print average market prices
     print("\nMean Values:")
     print(f"mean_lD_avg  = {mean_lD_avg:.6f}")
     print(f"mean_lR_avg  = {mean_lR_avg:.6f}")
-    print(f"mean_lIB_avg = {mean_lIB_avg:.6f} (used to calculate pos. and neg. imb. prices)")
+    print(f"mean_lPIB_avg  = {mean_lPIB_avg:.6f}")
+    print(f"mean_lNIB_avg  = {mean_lNIB_avg:.6f}")
     # Print average per intraday market
     for i in instance.IM:
         print(f"mean_lI_avg for IM[{i}] = {mean_lI_avg_per_market[i]:.6f}")
     # Print global average for intraday market
     print(f"mean_lI_global_avg (all IMs combined) = {mean_lI_global_avg:.6f}")
-    # Print average imbalances prices
-    print(f"mean_lPIB_avg  = {mean_lPIB_avg:.6f}")
-    print(f"mean_lNIB_avg  = {mean_lNIB_avg:.6f}")
+
     
 
     # =============================================================================
